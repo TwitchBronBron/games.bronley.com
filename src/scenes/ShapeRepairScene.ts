d75202209a1be63d6df4706e7fc9c593e799385b
Bronley Plumb <bronley@gmail.com>
2023-03-30 17:22:34 -0400

    Make it way harder

--- src/scenes/ShapeRepairScene.ts
+++ src/scenes/ShapeRepairScene.ts
@@ -11,8 +11,8 @@ export default class ShapeRepairScene extends Phaser.Scene {
   }
 
   private resetPositionSpeed = 4500;
-  private quadrantColumnCount = 3;
-  private quadrantRowCount = 2;
+  private quadrantColumnCount = 6;
+  private quadrantRowCount = 4;
 
   private distinctColors = [0xe6194B, 0x3cb44b, 0xffe119, 0x4363d8, 0xf58231, 0x911eb4, 0x42d4f4, 0xf032e6, 0xbfef45, 0xfabed4, 0x469990, 0xdcbeff, 0x9A6324, 0xfffac8, 0x800000, 0xaaffc3, 0x808000, 0xffd8b1, 0x000075, 0xa9a9a9, 0xffffff, 0x000000];
 
@@ -209,7 +209,7 @@ export default class ShapeRepairScene extends Phaser.Scene {
   private createShapePair(color: number, quadrant1: Quadrant, quadrant2: Quadrant) {
     //get a random degree that's within a range
     const cutDegree = getRandomIntInclusive(30, 120);
-    const radius = Math.min(quadrant1.width, quadrant1.height) / 3;
+    const radius = Math.min(quadrant1.width, quadrant1.height) / 2.1;
 
     const bigger = this.createCirclePart(color, radius, 0, cutDegree);
     this.makeDraggable(bigger);
